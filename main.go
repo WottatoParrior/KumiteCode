03d1e4c1bc7df64d7f42124097e53285d2fc5d86
WottatoParrior <mpovolakoulakos@gmail.com>
2022-05-14 20:08:27 +0300

    Initialize the app

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/sample", samplePage)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to KumiteCode \n")
+	fmt.Fprintf(w, "The place to interview potential candidates using Code Review \n")
+}
+
+func samplePage(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	fmt.Println("KumiteCode is up and running")
+	handleRequests()
+}
